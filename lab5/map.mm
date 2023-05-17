1906feaaee42497a13ca4be0bc7ce3074d1faedd
TheRunuwayDinosaur <93431648+TheRunuwayDinosaur@users.noreply.github.com>
2023-05-17 15:39:02 +0300

    Add files via upload

---
+++ lab5/map.mm
@@ -0,0 +1,256 @@
+<map version="freeplane 1.11.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Защита информации&#xa;Обеспечение информационной безопасности в организации&#xa;Основные термины и определения" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1684324933325" STYLE="oval">
+<font SIZE="20"/>
+<hook NAME="MapStyle" zoom="0.269">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Область применения" LOCALIZED_STYLE_REF="default" POSITION="bottom_or_right" ID="ID_1328816301" CREATED="1684321870939" MODIFIED="1684325177438" HGAP_QUANTITY="160.4 pt" VSHIFT_QUANTITY="302.40001 pt">
+<edge COLOR="#ff0000"/>
+<font SIZE="20"/>
+</node>
+<node TEXT="Нормативные ссылки" POSITION="top_or_left" ID="ID_145809502" CREATED="1684322437147" MODIFIED="1684322487668">
+<edge COLOR="#ff00ff"/>
+<font SIZE="16"/>
+<node TEXT="ГОСТ 34,003" ID="ID_813895345" CREATED="1684322474951" MODIFIED="1684324958765" VSHIFT_QUANTITY="-16.8 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="ГОСТ 19781" ID="ID_1845899401" CREATED="1684322494069" MODIFIED="1684324960941" VSHIFT_QUANTITY="-13.8 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="ГОСТ Р 22.0.02" ID="ID_434180377" CREATED="1684322512315" MODIFIED="1684324963462" HGAP_QUANTITY="20.6 pt" VSHIFT_QUANTITY="9 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="ГОСТ Р ИСО 9000" ID="ID_214083959" CREATED="1684323466102" MODIFIED="1684324965606" VSHIFT_QUANTITY="19.2 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="ГОСТ Р ИСО 9001" ID="ID_712194410" CREATED="1684323481947" MODIFIED="1684324968003">
+<font SIZE="20"/>
+</node>
+<node TEXT="ГОСТ Р ИСО 14001" ID="ID_468719635" CREATED="1684323495401" MODIFIED="1684324970495">
+<font SIZE="20"/>
+</node>
+<node TEXT="ГОСТ Р ИСО/МЭК 13335-1" ID="ID_1755605651" CREATED="1684323513956" MODIFIED="1684324972764">
+<font SIZE="20"/>
+</node>
+<node TEXT="ГОСТ Р ИСО/МЭК 27001" ID="ID_1861262899" CREATED="1684323532925" MODIFIED="1684324974964">
+<font SIZE="20"/>
+</node>
+<node TEXT="ГОСТ Р 50922" ID="ID_468433433" CREATED="1684323600518" MODIFIED="1684324977135">
+<font SIZE="20"/>
+</node>
+<node TEXT="ГОСТ Р 51275" ID="ID_1778387111" CREATED="1684323617359" MODIFIED="1684324979867">
+<font SIZE="20"/>
+</node>
+<node TEXT="ГОСТ Р 51898" ID="ID_164840068" CREATED="1684323630151" MODIFIED="1684324982155" VSHIFT_QUANTITY="15 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="ГОСТ Р 52069.0" ID="ID_1443821738" CREATED="1684323647377" MODIFIED="1684324984522">
+<font SIZE="20"/>
+</node>
+</node>
+<node TEXT="Термины и определения" POSITION="bottom_or_right" ID="ID_728563240" CREATED="1684323668884" MODIFIED="1684324990917">
+<edge COLOR="#7c0000"/>
+<font SIZE="20"/>
+<node TEXT="Общие понятия" ID="ID_402221796" CREATED="1684323700720" MODIFIED="1684325049800" HGAP_QUANTITY="164 pt" VSHIFT_QUANTITY="-31.2 pt">
+<font SIZE="20"/>
+<node TEXT="Безопасность информации" ID="ID_1828349832" CREATED="1684323716541" MODIFIED="1684325161375" HGAP_QUANTITY="8 pt" VSHIFT_QUANTITY="-33 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="Безопасность ифнормационных технологий" ID="ID_1714515107" CREATED="1684323730995" MODIFIED="1684325159294">
+<font SIZE="20"/>
+</node>
+<node TEXT="Информационная сфера" ID="ID_1647088393" CREATED="1684323754157" MODIFIED="1684325157670">
+<font SIZE="20"/>
+</node>
+<node TEXT="Информационная инфракструктура" ID="ID_1656604635" CREATED="1684323764238" MODIFIED="1684325155806">
+<font SIZE="20"/>
+</node>
+<node TEXT="Объект информатизации" ID="ID_1540903734" CREATED="1684323867213" MODIFIED="1684325154153">
+<font SIZE="20"/>
+</node>
+<node TEXT="Активы организации" ID="ID_1915288172" CREATED="1684323902770" MODIFIED="1684325152222">
+<font SIZE="20"/>
+</node>
+<node TEXT="Ресурс системы обработки информации" ID="ID_1697854679" CREATED="1684323929795" MODIFIED="1684325074494">
+<font SIZE="20"/>
+</node>
+<node TEXT="Информационный процесс" ID="ID_1872986083" CREATED="1684323946612" MODIFIED="1684325072793">
+<font SIZE="20"/>
+</node>
+<node TEXT="Информациооная технология" ID="ID_1362954645" CREATED="1684324078305" MODIFIED="1684325070968">
+<font SIZE="20"/>
+</node>
+<node TEXT="Техническое обеспечение автоматизированной системы" ID="ID_668223562" CREATED="1684324089594" MODIFIED="1684325069097">
+<font SIZE="20"/>
+</node>
+<node TEXT="Программное обеспечение автоматизированой системы" ID="ID_625513669" CREATED="1684324113631" MODIFIED="1684325067604">
+<font SIZE="20"/>
+</node>
+<node TEXT="Информационное обеспечение автоматизированной системы" ID="ID_1975357921" CREATED="1684324130298" MODIFIED="1684325065894">
+<font SIZE="20"/>
+</node>
+<node TEXT="Услуга" ID="ID_248886179" CREATED="1684324143942" MODIFIED="1684325064003">
+<font SIZE="20"/>
+</node>
+<node TEXT="Услуги информационных технологий" ID="ID_200341558" CREATED="1684324151973" MODIFIED="1684325061928">
+<font SIZE="20"/>
+</node>
+<node TEXT="Критически важная система информационной структуры" ID="ID_1495616449" CREATED="1684324167187" MODIFIED="1684325059925">
+<font SIZE="20"/>
+</node>
+<node TEXT="Критический объект" ID="ID_1871234370" CREATED="1684324195347" MODIFIED="1684325058205">
+<font SIZE="20"/>
+</node>
+<node TEXT="Информационная система персональных данных" ID="ID_63949737" CREATED="1684324209165" MODIFIED="1684325055882">
+<font SIZE="20"/>
+</node>
+<node TEXT="Персональные данные" ID="ID_1507574802" CREATED="1684324230348" MODIFIED="1684325054060">
+<font SIZE="20"/>
+</node>
+<node TEXT="Автоматизированная система в защищенном исполнении" ID="ID_1319581114" CREATED="1684324308171" MODIFIED="1684325051730" HGAP_QUANTITY="15.8 pt">
+<font SIZE="20"/>
+</node>
+</node>
+<node TEXT="Термины, относящиеся к объекту защиты информации" ID="ID_408651136" CREATED="1684324474256" MODIFIED="1684324993304" HGAP_QUANTITY="7.4 pt" VSHIFT_QUANTITY="-159.6 pt">
+<font SIZE="20"/>
+<node TEXT="Информационная безопасность организации" ID="ID_504847040" CREATED="1684324489980" MODIFIED="1684325044181" HGAP_QUANTITY="11.6 pt" VSHIFT_QUANTITY="-6 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="Объект защиты информации" ID="ID_1504936610" CREATED="1684324504383" MODIFIED="1684325042487" HGAP_QUANTITY="208.4 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="Защищаемый процесс (информационной технологии)" ID="ID_716618346" CREATED="1684324510532" MODIFIED="1684325040621" HGAP_QUANTITY="270.20001 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="Нарушение информационной безопасности организации" ID="ID_606583153" CREATED="1684324526998" MODIFIED="1684325038861" HGAP_QUANTITY="281.00001 pt" VSHIFT_QUANTITY="30 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="Чрезвычайная ситуация" ID="ID_160058025" CREATED="1684324540445" MODIFIED="1684325037078" HGAP_QUANTITY="301.40001 pt" VSHIFT_QUANTITY="16.8 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="Опасная ситуация" ID="ID_375865644" CREATED="1684324552332" MODIFIED="1684325035338" HGAP_QUANTITY="323.00001 pt" VSHIFT_QUANTITY="37.2 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="Инцидент информационной безопасности" ID="ID_154313726" CREATED="1684324563635" MODIFIED="1684325015823" HGAP_QUANTITY="226.40001 pt" VSHIFT_QUANTITY="-47.4 pt">
+<font SIZE="20"/>
+<node TEXT="Утрата услуг, оборудования или устройств" ID="ID_1948601925" CREATED="1684324588402" MODIFIED="1684325018249" VSHIFT_QUANTITY="-30 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="Системные сбои или перегрузки" ID="ID_960603487" CREATED="1684324605173" MODIFIED="1684325020262">
+<font SIZE="20"/>
+</node>
+<node TEXT="Ошибки пользователей" ID="ID_509628781" CREATED="1684324621150" MODIFIED="1684325022249">
+<font SIZE="20"/>
+</node>
+<node TEXT="Несоблюдение политики или рекомендаций по ИБ" ID="ID_850978644" CREATED="1684324632095" MODIFIED="1684325024192">
+<font SIZE="20"/>
+</node>
+<node TEXT="Нарушение физических мер защиты" ID="ID_1272378295" CREATED="1684324670232" MODIFIED="1684325026879">
+<font SIZE="20"/>
+</node>
+<node TEXT="Неконтролируемые изменения систем" ID="ID_1946111105" CREATED="1684324679823" MODIFIED="1684325028852">
+<font SIZE="20"/>
+</node>
+<node TEXT="Сбои программного обеспечения и отказы технических средств" ID="ID_653496430" CREATED="1684324691060" MODIFIED="1684325030854">
+<font SIZE="20"/>
+</node>
+<node TEXT="Нарушение правил доступа" ID="ID_502667244" CREATED="1684324699732" MODIFIED="1684325032858">
+<font SIZE="20"/>
+</node>
+</node>
+<node TEXT="Событие" ID="ID_304705517" CREATED="1684324710702" MODIFIED="1684324999657" HGAP_QUANTITY="116 pt" VSHIFT_QUANTITY="-14.4 pt">
+<font SIZE="20"/>
+<node TEXT="Характер, вероятность и последствия события могут быть не полностью известны" ID="ID_96244105" CREATED="1684324771275" MODIFIED="1684325002525" VSHIFT_QUANTITY="-75 pt">
+<font SIZE="20"/>
+</node>
+<node TEXT="Событие может возникать один или несколько раз" ID="ID_1500790193" CREATED="1684324785050" MODIFIED="1684325004815">
+<font SIZE="20"/>
+</node>
+<node TEXT="Вероятность, связанная с событием, может быть оценена" ID="ID_553940209" CREATED="1684324793746" MODIFIED="1684325006804">
+<font SIZE="20"/>
+</node>
+<node TEXT="Событие может состоять из невозникновения одного или нескольких обстоятельств" ID="ID_920523913" CREATED="1684324806773" MODIFIED="1684325008839">
+<font SIZE="20"/>
+</node>
+<node TEXT="Непредсказуемое событие иногда называют &quot;инцидентом&quot;" ID="ID_1754582814" CREATED="1684324816455" MODIFIED="1684325011278">
+<font SIZE="20"/>
+</node>
+<node TEXT="Событие, при котором не происходит никаких потерь, иногда называют предпосылкой к происшествию [инциденту], опасным состоянием, опасным стечением обстоятельств и т.д." ID="ID_1679793596" CREATED="1684324829646" MODIFIED="1684325013377">
+<font SIZE="20"/>
+</node>
+</node>
+<node TEXT="Риск" FOLDED="true" ID="ID_301175573" CREATED="1684324725651" MODIFIED="1684324996047">
+<font SIZE="20"/>
+<node TEXT="ХААХХАСАРМАТКРУТОЙ" ID="ID_1570802563" CREATED="1684325216045" MODIFIED="1684325224107">
+<font SIZE="20"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
